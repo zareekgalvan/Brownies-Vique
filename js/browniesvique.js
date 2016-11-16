2e5d5509435817d1f7194657b6aa7051484188a0
Armando Galván <armandocgv@gmail.com>
2016-11-15 21:49:31 -0600

    Contact form created and now functional

--- js/browniesvique.js
+++ js/browniesvique.js
@@ -25,4 +25,42 @@ $(document).ready(function() {
 
 
     //Contacto
+    $("#csubmit").click(function(){
+        event.preventDefault();
+        if($("#cname").val() != "" && $("#cemail").val() != "" && $("#ccomment").val() != "")
+        {
+            var jsonData = {
+                "action" : 'CONTACT',
+                "name" : $("#cname").val(),
+                "email" : $("#cemail").val(),
+                "comment" : $("#ccomment").val()
+            };
+
+            $.ajax({
+                url : "http://localhost:8888/BrowniesVique/data/applicationLayer.php",
+                type : "POST",
+                data : jsonData,
+                dataType : "json",
+                contentType : "application/x-www-form-urlencoded",
+                success: function(jsonResponse){
+                    alert("Se ha enviado su duda/mensaje, \n¡Muchas gracias!");
+                    console.log(jsonResponse.status);
+                },
+                error : function(errorMessage){
+                    alert("Por el momento no ha sido posible enviar su mensaje,\nintente de nuevo.");
+                }
+            });
+
+
+            
+        }
+        else 
+        {
+            alert("Por favor llene todos los campos");
+        }
+        event.stopPropagation();
+    });
+
+
+    //Ordenes pasadas
 });
